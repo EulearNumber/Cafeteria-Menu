e48123d7890614d315fc86de56773f6c860abec2
EulearNumber <yigitoksuz35@gmail.com>
2023-04-05 03:31:37 +0300

    The code

--- home.c
+++ home.c
@@ -9,7 +9,8 @@ typedef struct normalMenu {
     char sideDish[20];
     char extra[20];
     int saleCount[3];
-}normalMenu;
+} normalMenu;
+
 typedef struct veganMenu {
     char date[20];
     char soup[20];
@@ -17,7 +18,8 @@ typedef struct veganMenu {
     char sideDish[20];
     char extra[20];
     int saleCount[3];
-}veganMenu;
+} veganMenu;
+
 typedef struct vegetarianMenu {
     char date[20];
     char soup[20];
@@ -25,70 +27,107 @@ typedef struct vegetarianMenu {
     char sideDish[20];
     char extra[20];
     int saleCount[3];
-}vegetarianMenu;
+} vegetarianMenu;
+
 typedef struct Cafeteria {
     char* month;
     normalMenu* normalmenu;
     veganMenu* veganMenu;
     vegetarianMenu* vegetarianMenu;
-}Cafeteria; 
-Cafeteria* cafeteria = NULL;
-void read_menu();
-int main() {
-  cafeteria->normalmenu = malloc(sizeof(normalMenu) * 31);
-  cafeteria->veganMenu = malloc(sizeof(veganMenu) * 31);
-  cafeteria->vegetarianMenu = malloc(sizeof(vegetarianMenu) * 31);
-  read_menu(cafeteria);
-  printf("%s", cafeteria->normalmenu[0].mainDish);
+} Cafeteria;
 
-  free(cafeteria->normalmenu);
-  free(cafeteria->veganMenu);
-  free(cafeteria->vegetarianMenu);
-  free(cafeteria);
+Cafeteria* read_menu(Cafeteria* cafeteria);
+Cafeteria* init(Cafeteria* cafeteria);
+int main() {
+    Cafeteria* cafeteria = NULL;
+    cafeteria = init(cafeteria);
+    cafeteria = read_menu(cafeteria);
+    printf("%s",cafeteria->normalmenu[0].mainDish);
+
+    free(cafeteria->normalmenu);
+    free(cafeteria->veganMenu);
+    free(cafeteria->vegetarianMenu);
+    free(cafeteria);
 }
 
-void read_menu()
+Cafeteria* init(Cafeteria* cafeteria) {
+    cafeteria = malloc(sizeof(Cafeteria));
+    cafeteria->normalmenu = calloc(31, sizeof(normalMenu));
+    cafeteria->veganMenu = calloc(31, sizeof(veganMenu));
+    cafeteria->vegetarianMenu = calloc(31, sizeof(vegetarianMenu));
+    cafeteria->month = NULL;
+    return cafeteria;
+}
+
+Cafeteria* read_menu(Cafeteria* cafeteria)
 {
   FILE* fp;
   char ch;
   char* token; 
-  char Split = ',';
-  char* arr[6];
-  int i = 0, normalIndex = 0, veganIndex = 0, vegetarianIndex = 0,j = 0;
+  char* Split = ",";
+  char** arr = (char**) malloc(6 * sizeof(char*));
+  int i, normalIndex = 0, veganIndex = 0, vegetarianIndex = 0,j,n;
   char buffer[255];
+  int a = 0;
+  for( a = 0; a < 6;a++)
+  {
+    
+	arr[a] = malloc(15*sizeof(char));
+    
+  }
+  
   fp = fopen("cafeteria_march_menu.csv", "r");
   while(fgets(buffer, 255, fp) != NULL) {
-      token = strtok(buffer, &Split);
-      i = 0;
+      token = strtok(buffer, Split);
+      i = 0, j = 0;
       while(token != NULL) {
         arr[i++] = token;
-        token = strtok(NULL, &Split);
+        token = strtok(NULL, Split);
       }
-      if(strcmp(arr[0], "Normal") == 0)
+ 			
+      for(n=0; n<5; n++) {
+        for(j=0; j<strlen(arr[n]); j++) {
+            if(arr[n][j] == '\"') {
+                strcpy(&arr[n][j], &arr[n][j+1]);
+                j--;
+            }
+        }
+    }
+
+      if(strcmp(arr[0],  "Normal" ) == 0)
       {
         strcpy((cafeteria->normalmenu[normalIndex]).date, arr[1]);
         strcpy((cafeteria->normalmenu[normalIndex]).soup, arr[2]);
         strcpy((cafeteria->normalmenu[normalIndex]).mainDish, arr[3]);
+        printf("Flag:1 This string is %s \n", cafeteria -> normalmenu[normalIndex].mainDish);
         strcpy((cafeteria->normalmenu[normalIndex]).sideDish, arr[4]);
         strcpy((cafeteria->normalmenu[normalIndex]).extra, arr[5]);
+        normalIndex++;
+
       }
-      else if (strcmp(arr[0], "Vegan") == 0)
+      else if (strcmp(arr[0], "Vegan") == 0 )
+      { 
+        printf("You are in else if 1 \n");
+        strcpy((cafeteria->vegetarianMenu[vegetarianIndex]).date, arr[1]);
+        strcpy((cafeteria->vegetarianMenu[vegetarianIndex]).soup, arr[2]);
+        strcpy((cafeteria->vegetarianMenu[vegetarianIndex]).mainDish, arr[3]);
+        strcpy((cafeteria->vegetarianMenu[vegetarianIndex]).sideDish, arr[4]);
+        strcpy((cafeteria->vegetarianMenu[vegetarianIndex]).extra, arr[5]);
+        vegetarianIndex++;
+      }
+      else if (strcmp(arr[0], "Vegetarian") == 0)
       {
+        printf("You are in else if 2 \n");
         strcpy((cafeteria->veganMenu[veganIndex]).date, arr[1]);
         strcpy((cafeteria->veganMenu[veganIndex]).soup, arr[2]);
         strcpy((cafeteria->veganMenu[veganIndex]).mainDish, arr[3]);
         strcpy((cafeteria->veganMenu[veganIndex]).sideDish, arr[4]);
         strcpy((cafeteria->veganMenu[veganIndex]).extra, arr[5]);
+        veganIndex++;
       }
-      else if (strcmp(arr[0], "Vegetarian") == 0)
-      { 
-        strcpy(cafeteria->vegetarianMenu->date, arr[1]);
-        strcpy(cafeteria->vegetarianMenu->soup, arr[2]);
-        strcpy(cafeteria->vegetarianMenu->mainDish, arr[3]);
-        strcpy(cafeteria->vegetarianMenu->sideDish, arr[4]);
-        strcpy(cafeteria->vegetarianMenu->extra, arr[5]);
-      }
+     
   }
-  
+  free(arr);
   fclose(fp);
+  return cafeteria;
 }
